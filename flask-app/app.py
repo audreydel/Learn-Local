328b50a132841513fb24bbc3eb1f6c17c1ffae9a
Faith Olopade <91458920+faitholopade@users.noreply.github.com>
2023-12-29 16:06:17 +0000

    added database setup and a model for bookings

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bookings.db'
+db = SQLAlchemy(app)
 
+class Booking(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    time = db.Column(db.Time, nullable=False)
+    number_of_people = db.Column(db.Integer, nullable=False)
 
-@app.route("/info")
-def data():
-    return ("Hello Learn Local")
-
-
-if __name__ =="__main__":
-    app.run(debug=True)
+    def __repr__(self):
+        return f"<Booking {self.date} at {self.time} for {self.number_of_people} people>"
